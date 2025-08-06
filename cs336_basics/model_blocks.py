8265ed99e079e406ca74b2d1f8206012957170bb
philicxie <philicxie@163.com>
2025-08-07 00:16:09 +0800

    rms norm completed

--- cs336_basics/model_blocks.py
+++ cs336_basics/model_blocks.py
@@ -12,3 +12,37 @@ class Linear(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return einsum(x, self.weight, '... d_in, d_out d_in -> ... d_out')
+
+
+class Embedding(nn.Module):
+    def __init__(self, num_embeddings: int, embedding_dim: int, device=None, dtype=None):
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.weight = nn.Parameter(torch.randn(num_embeddings, embedding_dim, device=device, dtype=dtype), requires_grad=True)
+
+    def set(self, weights):
+        self.weight = torch.nn.Parameter(weights.to(device=self.weight.device, dtype=torch.float32))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.weight[x]
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(d_model, device=device, dtype=dtype), requires_grad=True)
+        self.d_model = d_model
+        self.dtype = dtype
+        self.eps = eps
+
+    def apply(self, weight):
+        self.weight = torch.nn.Parameter(weight.to(device=self.weight.device, dtype=torch.float32))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.to(torch.float32)
+        norm = torch.linalg.norm(x, dim=-1, keepdim=True)
+        rms = torch.sqrt(norm ** 2 / self.d_model + self.eps)
+
+        result = einsum(x / rms, self.weight, '... d, d -> ... d')
+        return result.to(self.dtype)
--- tests/adapters.py
+++ tests/adapters.py
@@ -382,7 +382,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rmsnorm = RMSNorm(d_model, eps)
+    rmsnorm.apply(weights)
+    return rmsnorm.forward(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
